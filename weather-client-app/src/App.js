0ef64e494eb1e59703a196aff10f840b8a414727
rafalostaszewski <86609310+rafalostaszewski@users.noreply.github.com>
2021-10-06 18:41:17 +0200

    Created weather-client-app Features: Display actual weather in searched city

---
+++ weather-client-app/src/App.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+const api = {
+  base: "https://localhost:5001/api/weatherforecast/actual"
+}
+
+function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+    if (evt.key === "Enter"){
+      fetch(`${api.base}?city=${query}`)
+      .then(res => res.json())
+      .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+      });
+    }
+  }
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "Julay", "August", "Seprember", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wensday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
+  return (
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        {(typeof weather.main != "undefined") ? (
+          <div>
+            <div className="location-box">
+              <div className="location">{weather.name}, {weather.sys.country}</div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">{Math.round(weather.main.temp)}°C</div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : ('')}
+      </main>
+    </div>
+  );
+}
+
+export default App;
