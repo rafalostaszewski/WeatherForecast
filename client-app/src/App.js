ed1411d7ec1ecdd67d09b04e8e3d159e40c2550e
rafalostaszewski <86609310+rafalostaszewski@users.noreply.github.com>
2021-10-22 16:11:59 +0200

    Added delete-history-button to client-app

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -6,6 +6,8 @@ const api = {
   historyall: "/history/all"
 }
 
+
+
 function App() {
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState([]);
@@ -30,6 +32,12 @@ function App() {
     }
   }
 
+  function handleDeleteHistoryAll() {
+    // DELETE request using fetch inside useEffect React hook
+    fetch(`${api.base + api.historyall}`, { method: 'DELETE' })
+    setHistoryAll([]);
+  }
+
   return (
     <div className="app">
       <main>
@@ -37,45 +45,53 @@ function App() {
           <input
             type="text"
             className="search-bar"
-            placeholder="Search..."
+            placeholder="Type city..."
             onChange={e => setQuery(e.target.value)}
             value={query}
             onKeyPress={search}
           />
         </div>
 
-      {(typeof weather.main != "undefined") ? (
-        <div>
-          <div className="location-box">
-            <div className="location">{weather.name}, {weather.sys.country}</div>
-            <div className="date">
-              <Moment className="time" format="DD MMMM YYYY H:mm ">
-                {weather.date}
-              </Moment></div>
+        {(typeof weather.main != "undefined") ? (
+          <div>
+            <div className="location-box">
+              <div className="location">{weather.name}, {weather.sys.country}</div>
+              <div className="date">
+                <Moment className="time" format="DD MMMM YYYY H:mm ">
+                  {weather.date}
+                </Moment></div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">{Math.round(weather.main.temp)}°C</div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
           </div>
-          <div className="weather-box">
-            <div className="temp">{Math.round(weather.main.temp)}°C</div>
-            <div className="weather">{weather.weather[0].main}</div>
-          </div>
-        </div>
-      ) : ('')}
-      {(typeof weather.main != "undefined") ? (
+        ) : ('')}
+
+        {(typeof weather.main != "undefined") ? (
           <div className="history-box">
             <h3>History</h3>
-            <ul className="history-records">
-              {historyAll.map(d => (
-                <li className="history-record" key={d.dbid}>
-                  <div className="history-city">{d.name}, {d.sys.country}</div>
-                  <div className="history-temp">{Math.round(d.main.temp)}°C</div>
-                  <div className="history-sky">{d.weather[0].main}</div>
-                  <Moment className="history-time" format="DD MMMM YYYY H:mm ">
-                    {d.date}
-                  </Moment>
-                </li>
-              ))}
-            </ul>
+            {(typeof historyAll[0] != "undefined") ? (
+              <button type="button" className="delete-btn" onClick={() => handleDeleteHistoryAll()}>Clear All</button>
+            ) : ('')}
+            {(typeof historyAll[0] != "undefined") ? (
+              <div>
+                <ul className="history-records">
+                  {historyAll.map(d => (
+                    <li className="history-record" key={d.dbid}>
+                      <div className="history-city">{d.name}, {d.sys.country}</div>
+                      <div className="history-temp">{Math.round(d.main.temp)}°C</div>
+                      <div className="history-sky">{d.weather[0].main}</div>
+                      <Moment className="history-time" format="DD MMMM YYYY H:mm ">
+                        {d.date}
+                      </Moment>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ) : ('')}
           </div>
-      ) : ('')}
+        ) : ('')}
       </main>
     </div>
   );
